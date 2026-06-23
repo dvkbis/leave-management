15f964b5104476f27fe72b5262dd0bc402489b4a
ST1403-10 <ST1403-10@FormaTF.dom>
2026-06-23 11:41:09 +0200

    [playground] test enum in leave_request

--- app/playground/test_leave_request.py
+++ app/playground/test_leave_request.py
@@ -1,5 +1,5 @@
 from app.database.session import SessionLocal
-from app.models import LeaveRequest
+from app.models import LeaveRequest, LeaveRequestStatus
 from datetime import datetime
 
 sessions = SessionLocal()
@@ -7,7 +7,7 @@ sessions = SessionLocal()
 leave_request = LeaveRequest(
     start_date = datetime(2026, 8, 23),
     end_date = datetime(2026, 8, 28),
-    status = "submitted",
+    status = LeaveRequestStatus.SUBMITTED,
     requested_at = datetime.now(),
     employee_id = 1,
     leave_type_id = 1
@@ -16,4 +16,4 @@ leave_request = LeaveRequest(
 sessions.add(leave_request)
 sessions.commit()
 
-print(leave_request)
+print(leave_request.id)
